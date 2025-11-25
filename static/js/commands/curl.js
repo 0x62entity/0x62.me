1c677d3015afd84ec6fb5d7306412bcb560b5dbb
Benjamin Yang <benjamin.yang12@outlook.com>
2025-11-26 09:24:10 +1100

    better curl err handling

--- static/js/commands/curl.js
+++ static/js/commands/curl.js
@@ -1,7 +1,11 @@
 export async function curl(url) {
-  const res = await fetch(url);
-  if (!res.ok) {
-    return `<span class="text-red">${res.statusText}</span>`
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      return `<span class="text-red">${res.status}</span>`;
+    }
+    return `<pre>${(await res.text()).replaceAll('<', '&lt').replaceAll('>', '&gt')}</pre>`;
+  } catch (e) {
+      return `<span class="text-red">${e.toString()}</span>`;
   }
-  return `<pre>${(await res.text()).replaceAll('<', '&lt').replaceAll('>', '&gt')}</pre>`;
 }
