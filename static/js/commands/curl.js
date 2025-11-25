d596f6728e455caa5b362a8d84f8e125a5107b79
Benjamin Yang <benjamin.yang12@outlook.com>
2025-11-26 08:30:05 +1100

    err handling in curl

--- static/js/commands/curl.js
+++ static/js/commands/curl.js
@@ -1,4 +1,7 @@
 export async function curl(url) {
   const res = await fetch(url);
+  if (!res.ok) {
+    return `<span class="text-red">${res.statusText}</span>`
+  }
   return `<pre>${await res.text()}</pre>`;
 }
